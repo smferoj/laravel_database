313a0ca7315a6f9add0822f1c04e2792f354d26e
smferoj <sm.feroj@gmail.com>
2023-12-02 13:32:25 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1. User table
+=> php artisan make:migration create_users
+=>php artisan make:migration create_categories (one to many)
+=>php artisan make:migration create_customers
+=>php artisan make:migration create_products
+=>php artisan migrate:rollback
+=>php artisan make:migration create_invoices
+=> php artisan make:migration create_invoice_products
---
+++ database/migrations/2023_12_02_065652_create_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('user_id')->references('id')->on('users')->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreign('category_id')->references('id')->on('categories')->restrictOnDelete()->cascadeOnUpdate();
+            $table->string('name', 50);
+            $table->string('price', 50);
+            $table->string('unit', 50);
+            $table->string('img_url', 50);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
